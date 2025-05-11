f20561c4771085d04bf2f515970a28b7ea95418a
Aleksey Klyuchinsky <klychinskyia@gmail.com>
2025-05-11 13:25:16 +0300

    created list 'books'

--- main.py
+++ main.py
@@ -4,5 +4,19 @@ import uvicorn
 app = FastAPI()
 
 
+books = [
+    {
+        "id": 1,
+        "title": "Асинхронность в Python",
+        "author": "Мэттью",
+    },
+    {
+        "id": 2,
+        "title": "Backend разработка в Python",
+        "author": "Артём",
+    },
+]
+
+
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
