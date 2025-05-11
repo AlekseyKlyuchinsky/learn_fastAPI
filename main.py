49839baaa43df0cdb0a4b5f92317f7cbe76278be
Aleksey Klyuchinsky <klychinskyia@gmail.com>
2025-05-11 13:33:50 +0300

    create function 'get_book'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
 app = FastAPI()
@@ -21,6 +21,14 @@ books = [
 @app.get('/books')
 def read_books():
     return books
+
+
+@app.get('/books/{book_id}')
+def get_book(book_id: int):
+    for book in books:
+        if book['id'] == book_id:
+            return book
+    raise HTTPException(status_code=404)
 
 
 if __name__ == '__main__':
